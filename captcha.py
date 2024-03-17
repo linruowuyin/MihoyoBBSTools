fc49d34f65a13a80036844804be67467db61e892
linruowuyin <63692049+linruowuyin@users.noreply.github.com>
2024-03-17 20:46:06 +0800

    Update captcha.py

--- captcha.py
+++ captcha.py
@@ -1,9 +1,25 @@
 from request import http
-
-
+#使用人人打码，需要自己注册付费使用
 def game_captcha(gt: str, challenge: str):
-    return None  # 失败返回None 成功返回validate
-
+    rep = http.post(
+        url="http://api.rrocr.com/api/recognize.html",
+        data={
+            "appkey": "",
+            "gt": gt,
+            "challenge": challenge,
+            "referer": "https://api-takumi.mihoyo.com/event/luna/sign"
+        }
+    ).json()
+    return rep["data"]["validate"]
 
 def bbs_captcha(gt: str, challenge: str):
-    return None
+    rep = http.post(
+        url="http://api.rrocr.com/api/recognize.html",
+        data={
+            "appkey": "",
+            "gt": gt,
+            "challenge": challenge,
+            "referer": "https://bbs-api.miyoushe.com/apihub/app/api/signIn"
+        }
+    ).json()
+    return rep["data"]["validate"]
